87a9b726765376b802155ee738ddc2383e4a050a
leon <voidest@hotmail.com>
2025-09-30 18:57:30 +0800

    close warnings

--- include/globals.h
+++ include/globals.h
@@ -5,6 +5,7 @@
 // (See config file of driver)
 #include "tomud.h"
 #pragma save_binary
+#pragma no_warnings
 //added by Beeby @ DTXY for TOPTEN
 #define TOPTEN_RICH     "/data/topten/rich.o"
 #define TOPTEN_PKER     "/data/topten/pker.o"
